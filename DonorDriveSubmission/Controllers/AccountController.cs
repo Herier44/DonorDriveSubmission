23f73e63a17b64450eaa9807957cc25de6136b04
Herier44 <djherier@yahoo.com>
2022-06-11 22:48:47 -0400

    Added an email package and updated form submission

---
+++ DonorDriveSubmission/Controllers/AccountController.cs
@@ -0,0 +1,17 @@
+﻿using Culture_Shock.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DonorDriveSubmission.Controllers
+{
+    public class AccountController : Controller
+    {
+        public async Task<IActionResult> AddUsersName(User user)
+        {
+            return View("~/Views/Home/EmailSent.cshtml", user);
+        }
+    }
+}
--- DonorDriveSubmission/Models/User.cs
+++ DonorDriveSubmission/Models/User.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,10 +8,15 @@ namespace Culture_Shock.Models
 {
     public class User
     {
+        [Required]
         public string UserName { get; set; }
-        public string Email { get; set; } 
+        [Required]
+        public string Email { get; set; }
+        [Required]
         public string FirstName { get; set; }
+        [Required]
         public string LastName { get; set; }
+        [Required]
         public DateTime JoinDate { get; set; }
 
     }
