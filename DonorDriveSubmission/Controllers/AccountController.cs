5f0b28e536f814faa7fd62b214120e24a0c8f70c
Herier44 <djherier@yahoo.com>
2022-06-12 12:57:37 -0400

    sending email setup

--- DonorDriveSubmission/Controllers/AccountController.cs
+++ DonorDriveSubmission/Controllers/AccountController.cs
@@ -1,4 +1,6 @@
 ﻿using Culture_Shock.Models;
+using DonorDriveSubmission.Models;
+//using FluentEmail.Core;
 using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
@@ -11,23 +13,50 @@ namespace DonorDriveSubmission.Controllers
 {
     public class AccountController : Controller
     {
-        public async Task<IActionResult> AddUsersName(User user)
+        public async Task AddUsersNameAsync(User user)
         {
+            var smtp = GetSmtpClient();
 
-            return View("~/Views/Home/EmailSent.cshtml", user);
+            var email = BuildEmail(user);
+
+            sendEmail(user, smtp, email);
         }
 
         public SmtpClient GetSmtpClient()
         {
             var smtp = new SmtpClient
             {
+                DeliveryMethod = SmtpDeliveryMethod.Network,
                 Host = "smtp.gmail.com",
                 Port = 587,
+                EnableSsl = true,
                 UseDefaultCredentials = false,
-                Credentials = new NetworkCredential("herier44@gmail.com", "password")
+                Credentials = new NetworkCredential("herier44@gmail.com", "password")//remember to not push my password, put your credentials in to test
             };
 
             return smtp;
         }
+
+
+        public Email BuildEmail(User user)
+        {
+            var email = new Email
+            {
+                SendTo = user.Email,
+                SendFrom = "herier44@gmail.com",
+                Body = "This is a test body.",
+                Subject = "This is a test email."
+            };
+
+            return email;
+        }
+
+        public IActionResult sendEmail(User user, SmtpClient smtp, Email email)
+        {
+            smtp.Send(email.SendFrom, email.SendTo, email.Subject, email.Body);
+
+            return View("~/Views/Home/EmailSent.cshtml", user);
+        }
+
     }
 }
---
+++ DonorDriveSubmission/Models/Email.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DonorDriveSubmission.Models
+{
+    public class Email
+    {
+        public string SendFrom { get; set; }
+        public string SendTo { get; set; }
+        public string Body { get; set; }
+        public string Subject { get; set; }
+
+    }
+}
